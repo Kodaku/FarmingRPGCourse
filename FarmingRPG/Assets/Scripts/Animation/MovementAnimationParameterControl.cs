6a5be717f3e1497ce39d60222164e7953ed20423
Axel_Mastroianni <a.mastroianni@studenti.unibs.it>
2022-04-10 09:31:48 +0200

    Initial Commit

---
+++ FarmingRPG/Assets/Scripts/Animation/MovementAnimationParameterControl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class MovementAnimationParameterControl : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        EventHandler.MovementEvent += SetAnimationParameters;
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.MovementEvent -= SetAnimationParameters;
+    }
+
+    private void SetAnimationParameters(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+        ToolEffect toolEffect,
+        bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingTooDown,
+        bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+        bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+        bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+        bool isIdleUp, bool isIdleDown, bool isIdleLeft, bool isIdleRight)
+    {
+        animator.SetFloat(Settings.xInput, inputX);
+        animator.SetFloat(Settings.yInput, inputY);
+        animator.SetBool(Settings.isWalking, isWalking);
+        animator.SetBool(Settings.isRunning, isRunning);
+
+        animator.SetInteger(Settings.toolEffect, (int)toolEffect);
+
+        if (isUsingToolRight)
+            animator.SetTrigger(Settings.isUsingToolRight);
+        if (isUsingToolLeft)
+            animator.SetTrigger(Settings.isUsingToolLeft);
+        if (isUsingToolUp)
+            animator.SetTrigger(Settings.isUsingToolUp);
+        if (isUsingTooDown)
+            animator.SetTrigger(Settings.isUsingToolDown);
+        if (isLiftingToolRight)
+            animator.SetTrigger(Settings.isLiftingToolRight);
+        if (isLiftingToolLeft)
+            animator.SetTrigger(Settings.isLiftingToolLeft);
+        if (isLiftingToolUp)
+            animator.SetTrigger(Settings.isLiftingToolUp);
+        if (isLiftingToolDown)
+            animator.SetTrigger(Settings.isLiftingToolDown);
+        if (isPickingRight)
+            animator.SetTrigger(Settings.isPickingRight);
+        if (isPickingLeft)
+            animator.SetTrigger(Settings.isPickingLeft);
+        if (isPickingUp)
+            animator.SetTrigger(Settings.isPickingUp);
+        if (isPickingDown)
+            animator.SetTrigger(Settings.isPickingDown);
+        if (isSwingingToolRight)
+            animator.SetTrigger(Settings.isSwingingToolRight);
+        if (isSwingingToolLeft)
+            animator.SetTrigger(Settings.isSwingingToolLeft);
+        if (isSwingingToolUp)
+            animator.SetTrigger(Settings.isSwingingToolUp);
+        if (isSwingingToolDown)
+            animator.SetTrigger(Settings.isSwingingToolDown);
+
+        if (isIdleUp)
+            animator.SetTrigger(Settings.idleUp);
+        if (isIdleDown)
+            animator.SetTrigger(Settings.idleDown);
+        if (isIdleLeft)
+            animator.SetTrigger(Settings.idleLeft);
+        if (isIdleRight)
+            animator.SetTrigger(Settings.idleRight);
+    }
+
+    private void AnimationEventPlayFootstepSound()
+    {
+
+    }
+}
---
+++ FarmingRPG/Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,34 @@
+﻿public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingTooDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool isIdleUp, bool isIdleDown, bool isIdleLeft, bool isIdleRight);
+
+public static class EventHandler
+{
+    // Movement Event
+    public static event MovementDelegate MovementEvent;
+
+    // Movement Event Call For Publishers
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingTooDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool isIdleUp, bool isIdleDown, bool isIdleLeft, bool isIdleRight)
+    {
+        if(MovementEvent != null)
+        {
+            MovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingTooDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                isIdleUp, isIdleDown, isIdleLeft, isIdleRight);
+        }
+    }
+}
