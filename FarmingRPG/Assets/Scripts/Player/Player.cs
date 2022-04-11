758c0f9ca6e33368dd38501f06c0738fc0eb3f54
Axel_Mastroianni <a.mastroianni@studenti.unibs.it>
2022-04-11 15:39:22 +0200

    Basic TileMap

--- FarmingRPG/Assets/Scripts/Player/Player.cs
+++ FarmingRPG/Assets/Scripts/Player/Player.cs
@@ -1,8 +1,161 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : SingletonMonobehaviour<Player>
 {
-    
+    // Movement Parameters
+    private float inputX;
+    private float inputY;
+    private bool isWalking;
+    private bool isRunning;
+    private bool isIdle;
+    private bool isCarrying = false;
+    private ToolEffect toolEffect = ToolEffect.none;
+    private bool isUsingToolRight;
+    private bool isUsingToolLeft;
+    private bool isUsingToolUp;
+    private bool isUsingToolDown;
+    private bool isLiftingToolRight;
+    private bool isLiftingToolLeft;
+    private bool isLiftingToolUp;
+    private bool isLiftingToolDown;
+    private bool isSwingingToolRight;
+    private bool isSwingingToolLeft;
+    private bool isSwingingToolUp;
+    private bool isSwingingToolDown;
+    private bool isPickingRight;
+    private bool isPickingLeft;
+    private bool isPickingUp;
+    private bool isPickingDown;
+
+    private Rigidbody2D rigidBody2D;
+
+    private Direction playerDirection;
+
+    private float movementSpeed;
+
+    private bool _playerInputIsDisabled = false;
+    public bool PlayerInputIsDisabled { get => _playerInputIsDisabled; set => _playerInputIsDisabled = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        #region Player Input
+
+        ResetAnimationTriggers();
+
+        PlayerMovementInput();
+
+        PlayerWalkInput();
+
+        EventHandler.CallMovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                false, false, false, false);
+
+        #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void ResetAnimationTriggers()
+    {
+        toolEffect = ToolEffect.none;
+        isUsingToolRight = false;
+        isUsingToolLeft = false;
+        isUsingToolUp = false;
+        isUsingToolDown = false;
+        isLiftingToolRight = false;
+        isLiftingToolLeft = false;
+        isLiftingToolUp = false;
+        isLiftingToolDown = false;
+        isSwingingToolRight = false;
+        isSwingingToolLeft = false;
+        isSwingingToolUp = false;
+        isSwingingToolDown = false;
+        isPickingRight = false;
+        isPickingLeft = false;
+        isPickingUp = false;
+        isPickingDown = false;
+    }
+
+    private void PlayerMovementInput()
+    {
+        inputY = Input.GetAxisRaw("Vertical");
+        inputX = Input.GetAxisRaw("Horizontal");
+
+        // Pythagora's theorem to move diagonally if pressing both x and y
+        if(inputY != 0 && inputX != 0)
+        {
+            inputX = inputX * 0.71f;
+            inputY = inputY * 0.71f;
+        }
+
+        if(inputX != 0 || inputY != 0)
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+
+            // Capture player direction for save game
+            if(inputX < 0)
+            {
+                playerDirection = Direction.left;
+            }
+            else if(inputX > 0)
+            {
+                playerDirection = Direction.right;
+            }
+            else if(inputY < 0)
+            {
+                playerDirection = Direction.down;
+            }
+            else if(inputY > 0)
+            {
+                playerDirection = Direction.up;
+            }
+        }
+        else if(inputX == 0 && inputY == 0)
+        {
+            isRunning = false;
+            isWalking = false;
+            isIdle = true;
+        }
+    }
+
+    private void PlayerWalkInput()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isRunning = false;
+            isWalking = true;
+            isIdle = false;
+            movementSpeed = Settings.walkingSpeed;
+        }
+        else
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+        }
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 move = new Vector2(inputX * movementSpeed * Time.deltaTime, inputY * movementSpeed * Time.deltaTime);
+
+        rigidBody2D.MovePosition(rigidBody2D.position + move);
+    }
 }
