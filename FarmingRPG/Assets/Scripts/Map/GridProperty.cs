5dc3521b2a9c7ae7533de3464780efebd5aebbf3
Axel_Mastroianni <a.mastroianni@studenti.unibs.it>
2022-05-12 16:58:27 +0200

    Grid Properties Painting

---
+++ FarmingRPG/Assets/Scripts/Map/GridProperty.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GridProperty
+{
+    public GridCoordinate gridCoordinate;
+    public GridBoolProperty gridBoolProperty;
+    public bool gridBoolValue = false;
+
+    public GridProperty(GridCoordinate gridCoordinate, GridBoolProperty gridBoolProperty, bool gridBoolValue)
+    {
+        this.gridCoordinate = gridCoordinate;
+        this.gridBoolProperty = gridBoolProperty;
+        this.gridBoolValue = gridBoolValue;
+    }
+}
